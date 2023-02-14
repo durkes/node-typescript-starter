97c1bc59fdafc82ef4a2acd369f8bddd878059b6
durkes <durkes@users.noreply.github.com>
2023-02-13 18:39:48 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// typescript deep dive book
+// https://github.com/basarat/typescript-book
+
+// compile from CLI: npm run build (will compile on each save)
+
+import helper from './helper.js'; // use .js or .cjs extension for imports
+
+const output = helper('It\'s working!');
+console.log(output);
